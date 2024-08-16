fcaf28dfdd9ad7b0da39d7b0aa9013131d5624c0
Anupam Singh <96816312+anupam-singh-rajput@users.noreply.github.com>
2024-08-16 15:22:02 -0700

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,20 +1,127 @@
+// import React, { useEffect, useState } from 'react';
+// import './Hidescollbar.css';
+// import { useNavigate } from 'react-router-dom';
+// import Cookies from 'js-cookie';
+// import Chats from './Chats';
+// import Profile from './Profile';
+// const apiUrl = process.env.REACT_APP_API_URL;
+
+// const Home = () => {
+//     const [friends, setFriends] = useState([]);
+//     const [selectedUser, setSelectedUser] = useState(null);
+//     const [username, setUsername] = useState('')
+//     const navigate = useNavigate();
+
+//     useEffect(() => {
+//         const token = Cookies.get('token');
+//         // Fetch friends from the backend
+//         const fetchFriends = async () => {
+//             try {
+//                 const response = await fetch(`${apiUrl}/api/users/getuser`, {
+//                     method: 'GET',
+//                     headers: {
+//                         'Content-Type': 'application/json',
+//                         'Authorization': `Bearer ${token}`
+//                     },
+//                     credentials: 'include',
+//                 });
+
+//                 const data = await response.json();
+//                 console.log(data);
+//                 setUsername(data.name);
+
+//                 // Check and set friends array
+//                 if (data.success && Array.isArray(data.friends)) {
+//                     setFriends(data.friends);
+//                 } else {
+//                     console.error('Unexpected response structure:', data);
+//                 }
+//             } catch (error) {
+//                 console.error('Error fetching friends:', error);
+//             }
+//         };
+
+//         fetchFriends();
+//     }, [navigate]);
+
+//     const handleUserClick = (email) => {
+//         setSelectedUser(email); // Set selected user email
+//     };
+
+//    function getUsername(){
+//     setUsername(this.name);
+//     // console.log(username);
+//    }
+//    console.log(username);
+//     return (
+
+//         <div className="bodyhome bg-gray-200 w-screen h-screen fixed overflow-hidden ml-4">
+//             <div className="header flex justify-between mt-2 mb-2 ">
+//                 <div className="header-search ml-4">
+//                     <button 
+//                         type="submit"  
+//                         className="bg-gradient-to-tr from-purple-900 to-indigo-900 text-white px-4 py-2 focus:outline-none w-80"
+//                         onClick={() => { navigate("/search") }}
+//                     >
+//                         Search
+//                     </button>
+//                 </div>
+//                 <div className="header-search ml-4">
+//                     {username}
+//                 </div>
+//                 <div className="header-profile">
+//                     <button 
+//                         type="submit"  
+//                         className="bg-gradient-to-tr text-white from-purple-900 to-indigo-900 text-1rem px-4 py-2 focus:outline-none mr-8 w-32"
+//                         onClick={() => { navigate('/profile') }}
+//                     >
+//                         Profile
+//                     </button>
+//                 </div> 
+//             </div>  
+//             <div className="container w-screen h-[90%] flex justify-between">
+//                 <div className="left-container w-[21%] h-full bg-white scrollbar-hide overflow-y-scroll">
+//                     {Array.isArray(friends) && friends.map((friend) => (
+//                         <div 
+//                             key={friend.email} 
+//                             className={`user w-[100%] h-[100px] flex justify-center items-center border-b-2 border-indigo-900 pl-2 cursor-pointer ${
+//                                 selectedUser === friend.email ? 'bg-gradient-to-tr  from-purple-900 to-indigo-900 text-white text-base' : 'bg-white'
+//                             }`}
+//                             onClick={() => handleUserClick(friend.email)} // Set selected user on click
+//                         >
+//                             <div className="profile-pic w-[30%] h-[90%] bg-black rounded-full">
+//                                 <img src={friend.profilePhoto || "path_to_default_image"} alt="" />
+//                             </div>
+//                             <div className="username w-[70%] h-[90%] text-left pl-2 content-center">
+//                                 <p>{friend.name}</p>
+//                             </div>
+//                         </div>
+//                     ))}
+//                 </div>
+//                 <Chats selectedUser={selectedUser} /> {/* Pass selectedUser to Chats */}
+//             </div>
+//         </div>
+
+//     );
+// };
+// export default Home;
+
 import React, { useEffect, useState } from 'react';
 import './Hidescollbar.css';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import Chats from './Chats';
-import Profile from './Profile';
+
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const Home = () => {
     const [friends, setFriends] = useState([]);
     const [selectedUser, setSelectedUser] = useState(null);
-    const [username, setUsername] = useState('')
+    const [username, setUsername] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
         const token = Cookies.get('token');
-        // Fetch friends from the backend
         const fetchFriends = async () => {
             try {
                 const response = await fetch(`${apiUrl}/api/users/getuser`, {
@@ -30,7 +137,6 @@ const Home = () => {
                 console.log(data);
                 setUsername(data.name);
 
-                // Check and set friends array
                 if (data.success && Array.isArray(data.friends)) {
                     setFriends(data.friends);
                 } else {
@@ -45,63 +151,78 @@ const Home = () => {
     }, [navigate]);
 
     const handleUserClick = (email) => {
-        setSelectedUser(email); // Set selected user email
+        setSelectedUser(email);
     };
 
-   function getUsername(){
-    setUsername(this.name);
-    // console.log(username);
-   }
-   console.log(username);
-    return (
+   return (
+    <div className="bodyhome bg-gray-200 w-screen h-screen relative flex flex-col items-center overflow-hidden m-0">
+    <div className="header w-[100vw] h-[10vh] flex justify-between items-center  ">
+    <div className="header-search ml-[2vmin]">
+        <button 
+            type="submit"  
+            className="bg-gradient-to-tr from-purple-900 to-indigo-900 text-white px-[3vmin] py-[2vmin] focus:outline-none w-[20vmin]"
+            onClick={() => { navigate("/search") }}
+        >
+            Search
+        </button>
+    </div>
+    <div className="header-name">
+        {username}
+    </div>
+    <div className="header-profile">
+        <button 
+            type="submit"  
+            className="bg-gradient-to-tr text-white from-purple-900 to-indigo-900 text-[1rem] px-[3vmin] py-[2vmin] focus:outline-none mr-[2vmin] w-[11vmax]"
+            onClick={() => { navigate('/profile') }}
+        >
+            Profile
+        </button>
+    </div> 
+    </div>
 
-        <div className="bodyhome bg-gray-200 w-screen h-screen fixed overflow-hidden ml-4">
-            <div className="header flex justify-between mt-2 mb-2 ">
-                <div className="header-search ml-4">
-                    <button 
-                        type="submit"  
-                        className="bg-gradient-to-tr from-purple-900 to-indigo-900 text-white px-4 py-2 focus:outline-none w-80"
-                        onClick={() => { navigate("/search") }}
+        <div className="container w-[100vw] h-[90vh] flex justify-between gap-[1vh]">
+            <div className="left-container w-[21vw] h-full bg-white scrollbar-hide overflow-y-scroll">
+                {Array.isArray(friends) && friends.map((friend) => (
+                    <div 
+                        key={friend.email} 
+                        className={`user w-[100%] h-[12vh] flex justify-center items-center border-b-2 border-indigo-900 pl-[1vw] cursor-pointer ${
+                            selectedUser === friend.email ? 'bg-gradient-to-tr from-purple-900 to-indigo-900 text-white text-base' : 'bg-white'
+                        }`}
+                        onClick={() => handleUserClick(friend.email)} // Set selected user on click
                     >
-                        Search
-                    </button>
-                </div>
-                <div className="header-search ml-4">
-                    {username}
-                </div>
-                <div className="header-profile">
-                    <button 
-                        type="submit"  
-                        className="bg-gradient-to-tr text-white from-purple-900 to-indigo-900 text-1rem px-4 py-2 focus:outline-none mr-8 w-32"
-                        onClick={() => { navigate('/profile') }}
-                    >
-                        Profile
-                    </button>
-                </div> 
-            </div>  
-            <div className="container w-screen h-[90%] flex justify-between">
-                <div className="left-container w-[21%] h-full bg-white scrollbar-hide overflow-y-scroll">
-                    {Array.isArray(friends) && friends.map((friend) => (
-                        <div 
-                            key={friend.email} 
-                            className={`user w-[100%] h-[100px] flex justify-center items-center border-b-2 border-indigo-900 pl-2 cursor-pointer ${
-                                selectedUser === friend.email ? 'bg-gradient-to-tr  from-purple-900 to-indigo-900 text-white text-base' : 'bg-white'
-                            }`}
-                            onClick={() => handleUserClick(friend.email)} // Set selected user on click
-                        >
-                            <div className="profile-pic w-[30%] h-[90%] bg-black rounded-full">
-                                <img src={friend.profilePhoto || "path_to_default_image"} alt="" />
-                            </div>
-                            <div className="username w-[70%] h-[90%] text-left pl-2 content-center">
-                                <p>{friend.name}</p>
-                            </div>
+                        <div className="profile-pic w-[7vmax] h-[90%] bg-black rounded-full">
+                            <img src={friend.profilePhoto || "path_to_default_image"} alt="" />
                         </div>
-                    ))}
-                </div>
+                        <div className="username w-[70%] h-[90%] text-left pl-[1vw] content-center">
+                            <p>{friend.name}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="right-container w-[79vw]">
                 <Chats selectedUser={selectedUser} /> {/* Pass selectedUser to Chats */}
             </div>
         </div>
-    );
+    </div>
+);
+
+
 };
 
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
