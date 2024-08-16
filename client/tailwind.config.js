60621523d3e459807962c18148e8a9512f626e9b
Anupam Singh <96816312+anupam-singh-rajput@users.noreply.github.com>
2024-08-15 21:50:47 -0700

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,26 +1,10 @@
-// // tailwind.config.js
-// module.exports = {
-//   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// };
-
+// tailwind.config.js
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/index.html',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
   },
   plugins: [],
-  screens: {
-    'sm': '640px',
-    'md': '768px',
-    'lg': '1024px',
-    'xl': '1280px',
-    '2xl': '1536px',
-  },
-}
+};
+
+
